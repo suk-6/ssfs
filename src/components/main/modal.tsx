1199d5090f0c4931f119932c5a48f1903c2e736d
suk-6 <me@suk.kr>
2024-06-01 00:10:58 +0900

    feat: uploadStatus page

--- src/components/main/modal.tsx
+++ src/components/main/modal.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { FileUpload } from "./fileUpload";
 import { UploadStatus } from "@/misc/uploadStatus";
+import { uploadFile } from "@/utils/upload";
 
 export const MainModal = () => {
 	const [uploadStatus, setUploadStatus] = useState<UploadStatus>(
@@ -10,36 +11,21 @@ export const MainModal = () => {
 	);
 	const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-	const uploadFile = async () => {
-		if (!selectedFile) return;
-
-		const password = (
-			document.getElementById("password") as HTMLInputElement
-		).value;
-		if (!password) {
-			alert("비밀번호를 입력하세요.");
-			return;
-		}
-
-		// Uploading Start
-		setUploadStatus(UploadStatus.Uploading);
-
-		console.log(selectedFile.name, selectedFile.size, selectedFile.type);
-	};
-
 	return (
 		<div className=" w-full h-full flex flex-col items-center justify-center gap-7">
-			<div className=" w-4/6 h-[500px] border border-gray-200 rounded-lg shadow-2xl">
-				<div className=" w-full h-full flex p-5">
-					<div className=" w-full h-full flex justify-center items-center">
-						<FileUpload
-							selectedFile={selectedFile}
-							setSelectedFile={setSelectedFile}
-						/>
+			{uploadStatus === UploadStatus.Idle && (
+				<div className=" w-4/6 h-[500px] border border-gray-200 rounded-lg shadow-2xl">
+					<div className=" w-full h-full flex p-5">
+						<div className=" w-full h-full flex justify-center items-center">
+							<FileUpload
+								selectedFile={selectedFile}
+								setSelectedFile={setSelectedFile}
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
-			{selectedFile && (
+			)}
+			{selectedFile && uploadStatus === UploadStatus.Idle && (
 				<div className=" w-4/6 h-[60px] flex flex-row gap-3">
 					<div className=" w-full h-full bg-white border flex justify-center items-center">
 						<input
@@ -52,7 +38,12 @@ export const MainModal = () => {
 					</div>
 					<button
 						className=" w-1/4 h-full bg-white hover:bg-gray-100 border text-gray-500 rounded-lg"
-						onClick={() => uploadFile()}
+						onClick={() =>
+							uploadFile({
+								selectedFile,
+								setUploadStatus,
+							})
+						}
 					>
 						업로드
 					</button>
