0e587e8d036b1d1897c827effdaa886bb0cc5e9e
suk-6 <me@suk.kr>
2024-06-04 16:34:29 +0900

    feat: Add lazy loading for images in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { ListView } from "@/components/list/listView";
 import { MainModal } from "@/components/main/modal";
+import { ListObjectsV2Output } from "@aws-sdk/client-s3";
+import { useState } from "react";
 
 export default function Home() {
+	const [s3Data, setS3Data] = useState<ListObjectsV2Output | null>(null);
+
 	return (
 		<main className=" w-screen h-screen">
-			<MainModal />
+			{s3Data ? (
+				<ListView contents={s3Data.Contents} />
+			) : (
+				<MainModal setS3Data={setS3Data} />
+			)}
 		</main>
 	);
 }
